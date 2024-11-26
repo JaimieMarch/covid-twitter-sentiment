eea271518943127ad109c1f3c3bb3bdc5a18b468
jRaimuu <liamsarj4@gmail.com>
2024-11-26 14:04:17 -0700

    Added sample script to fetch data

---
+++ src/visualization/streamlit.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+from pathlib import Path
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.getcwd(), '..')))
+
+from database import database_manager as dbm
+
+'''
+queries database to get the sentiment scores
+returns df
+'''
+def get_sentiment_data():
+    print("Retrieving sentiment scores table...")
+    table = "sentiment_scores"
+    query = f"""
+        SELECT * 
+        FROM "{table}" 
+    """
+    df = dbm.query_db(query)
+    return df
+
+
+def main():
+    df_sent = get_sentiment_data()
+    print(df_sent.head(15))
+
+
+
+if __name__ == '__main__':
+    main()
